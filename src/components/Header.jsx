d41c1d16c9717915a6f441949d1af4a566d10cfa
buzzBoy21 <buzzBoy021@gmail.com>
2025-04-25 14:46:18 +0200

    adding mobile support

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,22 @@ import { useBrowserDetection } from '../hook/useBrowserDetection';
 import style from './header.module.css';
 import { ImportFolders } from './importFolder/ImportFolders';
 import logo from './../assets/logo.svg';
-import { Button } from '@chakra-ui/react';
+import {
+   Button,
+   Drawer,
+   DrawerOverlay,
+   DrawerContent,
+   DrawerCloseButton,
+   DrawerHeader,
+   DrawerBody,
+   useDisclosure,
+} from '@chakra-ui/react';
+import { useState, useRef } from 'react';
+import hamburger from './../assets/hamburger.svg';
+
 export function Header({ handleMoreInfoButton = () => {}, handleConfigButton = () => {} }) {
+   const { isOpen, onOpen, onClose } = useDisclosure();
+   const btnRef = useRef();
    //return true if navigator is edge, chrome or opera
    const canIuseShowDirectoryPicker = useBrowserDetection();
    return (
@@ -16,10 +30,37 @@ export function Header({ handleMoreInfoButton = () => {}, handleConfigButton = (
             <Button colorScheme="gray" variant="outline" onClick={handleMoreInfoButton}>
                Best Practices
             </Button>
-            <Button colorScheme="gray" variant="outline" onClick={handleConfigButton}>
+            <Button colorScheme="gray" variant="outline" onClick={handleConfigButton} ref={btnRef}>
                Config
             </Button>
+
+            <button onClick={onOpen} className={style.hamburger}>
+               <img src={hamburger} alt="menu icon" style={{ height: '2em', width: 'auto' }} />
+            </button>
          </div>
+         <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
+            <DrawerOverlay />
+            <DrawerContent>
+               <DrawerCloseButton />
+               <DrawerHeader>Menu</DrawerHeader>
+
+               <DrawerBody display={'flex'} flexDirection={'column'} gap={'2em'}>
+                  <ImportFolders useShowDirectoryPicker={canIuseShowDirectoryPicker}>
+                     Import Folders
+                  </ImportFolders>
+                  <Button colorScheme="gray" variant="outline" onClick={handleMoreInfoButton}>
+                     Best Practices
+                  </Button>
+                  <Button
+                     colorScheme="gray"
+                     variant="outline"
+                     onClick={handleConfigButton}
+                     ref={btnRef}>
+                     Config
+                  </Button>
+               </DrawerBody>
+            </DrawerContent>
+         </Drawer>
       </header>
    );
 }
