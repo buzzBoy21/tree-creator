4c23a9155fc5c422cd8602f33b9fb5430d337ad4
buzzBoy21 <buzzBoy021@gmail.com>
2025-05-04 22:18:22 +0200

    erased unused component

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -5,11 +5,10 @@ import ConfigModal from './../components/ConfigModal/ConfigModal';
 import style from './../App.module.css';
 import { EditTree } from './../components/editTree/EditTree';
 import { Header } from './../components/Header';
-import { Suspense, useState, useTransition } from 'react';
+import { Suspense, useTransition } from 'react';
 import { useDisclosure } from '@chakra-ui/react';
 import EditTreeSkeleton from '../components/skeletons/EditTreeSkeleton';
 import { OutPutSkeleton } from '../components/skeletons/OutPutSkeleton';
-import ReRender from '../components/reRender/reRender';
 
 function MainLayout() {
    const { isOpen, onOpen, onClose } = useDisclosure();
@@ -22,33 +21,6 @@ function MainLayout() {
 
    return (
       <>
-         {/* <ConfigurationProvider>
-           
-            <Header
-               startTransition={startTransition}
-               handleMoreInfoButton={onOpenHelpModal}
-               handleConfigButton={onOpen}
-            />
-            {isPending ? (
-               <>
-                  <div className={style.containerPage}>
-                     <EditTreeSkeleton></EditTreeSkeleton>
-                     <OutPutSkeleton></OutPutSkeleton>
-                  </div>
-               </>
-            ) : (
-               <>
-                  <div className={style.containerPage}>
-                     <EditTree></EditTree>
-                     <OutPut></OutPut>
-
-                     <ConfigModal isOpen={isOpen} onClose={onClose}></ConfigModal>
-                     <HelpModal isOpen={isOpenHelpModal} onClose={onCloseHelpModal}></HelpModal>
-                  </div>
-               </>
-            )}
-         </ConfigurationProvider> */}
-
          <ConfigurationProvider>
             <Suspense
                fallback={
